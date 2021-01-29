a3ee10738e1f029641ce9ef433da412cddeb70fc
satyam Singhania <satyam.singhania123@gmail.com>
2021-01-29 13:45:37 +0530

    commited

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,83 +1,110 @@
-const express = require('express')
+const express = require('express');
 const Product = require('../models/productModel');
 const { getToken } = require('../utill/utill');
 const { isAuth, isAdmin } = require('../utill/auth');
-const { validateProductForm } = require('../utill/validator')
+const { validateProductForm } = require('../utill/validator');
 const expressAsyncHandler = require('express-async-handler');
 
- const router = express.Router();
+const router = express.Router();
 
- //get Products
-router.get("/",expressAsyncHandler(async (req, res)=>{
- const products  = await Product.find({});
- res.json(products) 
-}));
+//get Products
+router.get(
+	'/',
+	expressAsyncHandler(async (req, res) => {
+		console.log('object');
+		const products = await Product.find({});
+		return res.json(products);
+	})
+);
 
- //get Product
-router.get("/product/:id", expressAsyncHandler( async (req, res)=>{
- const product  = await Product.findOne({_id: req.params.id});
-if(product){
-    res.status(201).json(product) 
-}else{
-    res.status(404).json({message: 'Product not found'})
-    }
-}));
+//get Product
+router.get(
+	'/product/:id',
+	expressAsyncHandler(async (req, res) => {
+		const product = await Product.findOne({ _id: req.params.id });
+		if (product) {
+			res.status(201).json(product);
+		} else {
+			res.status(404).json({ message: 'Product not found' });
+		}
+	})
+);
 
 //Create Product
-router.post("/product", isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
-        const {valid, errors} = validateProductForm(req.body)
-        if(!valid) return res.status(401).json(errors)
-     const product  = new Product({
-         name: req.body.name,
-         price: req.body.price,
-         image: req.body.image,
-         brand: req.body.brand,
-         category: req.body.category,
-         countInStock: req.body.countInStock,
-         description: req.body.description,
-         rating: req.body.rating,
-         numReviews: req.body.numReviews,
-        })
-        const newProduct = await product.save();
-        if(newProduct){
-           return res.status(201).json({message : 'New Product Created',  data: newProduct})
-        }else{
-            return res.status(500).json({message: 'Error in creating product'})
-        }
-
-}))
+router.post(
+	'/product',
+	isAuth,
+	isAdmin,
+	expressAsyncHandler(async (req, res) => {
+		const { valid, errors } = validateProductForm(req.body);
+		if (!valid) return res.status(401).json(errors);
+		const product = new Product({
+			name: req.body.name,
+			price: req.body.price,
+			image: req.body.image,
+			brand: req.body.brand,
+			category: req.body.category,
+			countInStock: req.body.countInStock,
+			description: req.body.description,
+			rating: req.body.rating,
+			numReviews: req.body.numReviews,
+		});
+		const newProduct = await product.save();
+		if (newProduct) {
+			return res
+				.status(201)
+				.json({ message: 'New Product Created', data: newProduct });
+		} else {
+			return res.status(500).json({ message: 'Error in creating product' });
+		}
+	})
+);
 //Update Product
-router.put("/product/:id", isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
-    const productId = req.params.id;
-    const product = await Product.findById(productId);
-    if(product){
-        product._id = product._id;
-        product.name = req.body.name;
-        product.price = req.body.price;
-        product.image = req.body.image;
-        product.brand = req.body.brand;
-        product.category = req.body.category;
-        product.countInStock = req.body.countInStock;
-        product.description = req.body.description;
-        product.rating = product.rating;
-        product.numReviews = product.numReviews; 
-        const updatedProduct = await product.save()        
-        if(updatedProduct){
-            return res.status(201).json({message : 'Product Updated',  data: updatedProduct})
-            }
-        }
-           return res.status(500).json({message: 'Error in updating product'})
-        }))
+router.put(
+	'/product/:id',
+	isAuth,
+	isAdmin,
+	expressAsyncHandler(async (req, res) => {
+		const productId = req.params.id;
+		const product = await Product.findById(productId);
+		if (product) {
+			product._id = product._id;
+			product.name = req.body.name;
+			product.price = req.body.price;
+			product.image = req.body.image;
+			product.brand = req.body.brand;
+			product.category = req.body.category;
+			product.countInStock = req.body.countInStock;
+			product.description = req.body.description;
+			product.rating = product.rating;
+			product.numReviews = product.numReviews;
+			const updatedProduct = await product.save();
+			if (updatedProduct) {
+				return res
+					.status(201)
+					.json({ message: 'Product Updated', data: updatedProduct });
+			}
+		}
+		return res.status(500).json({ message: 'Error in updating product' });
+	})
+);
 
-        //Delete Product
-router.delete("/product/:id", isAuth, isAdmin, expressAsyncHandler(async (req, res) => {
-    const productId = req.params.id;
-    const deletedProduct = await Product.findById(productId);
-    if(deletedProduct){
-       await deletedProduct.remove()
-        return res.status(201).json({message : 'Product Deleted',  data: deletedProduct})
-    }
-       return res.status(500).json({ message: 'Error in deletion' })
-    }))
-        
-     module.exports = router
+//Delete Product
+router.delete(
+	'/product/:id',
+	isAuth,
+	isAdmin,
+	expressAsyncHandler(async (req, res) => {
+		const productId = req.params.id;
+		const deletedProduct = await Product.findById(productId);
+		if (deletedProduct) {
+			await deletedProduct.remove();
+			return res
+				.status(201)
+				.json({ message: 'Product Deleted', data: deletedProduct });
+		}
+		return res.status(500).json({ message: 'Error in deletion' });
+	})
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-const express =  require('express');
+const express = require('express');
 
 //mongo
-const config = require('./utill/config')
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const config = require('./utill/config');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const userRoute = require('./routes/userRoute')
-const productRoute = require('./routes/productRoute')
-const bodyParser = require('body-parser')
+const userRoute = require('./routes/userRoute');
+const productRoute = require('./routes/productRoute');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 const mongodburl = config.MONGODB_URL;
-mongoose.connect(mongodburl, {
-    useNewUrlParser : true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}).catch(error => console.log(error));
+mongoose
+	.connect(mongodburl, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.catch((error) => console.log(error));
 
-app.use('/api/users/', userRoute)
-app.use('/api/products/', productRoute)
+app.use('/api/users/', userRoute);
+app.use('/api/products/', productRoute);
 
-app.use((err, req, res, next) =>{
-    const status = err.name && (err.name = 'ValidationError') ? 400 : 500 ;
-    res.status(status).json({message: err.message})
-})
-app.listen( 5000, ()=> console.log('server Started at localhost 5000') )   
+app.use((err, req, res, next) => {
+	const status = err.name && (err.name = 'ValidationError') ? 400 : 500;
+	res.status(status).json({ message: err.message });
+});
+app.listen(5000, () => console.log('server Started at localhost 6000'));

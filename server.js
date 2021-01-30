8a973a873c84d84a751ea448f27dc9305032a87a
satyam Singhania <satyam.singhania123@gmail.com>
2021-01-30 15:16:04 +0530

    heroku

--- server.js
+++ server.js
@@ -30,4 +30,5 @@ app.use((err, req, res, next) => {
 	const status = err.name && err.name == 'ValidationError' ? 400 : 500;
 	res.status(status).json({ message: err.message });
 });
+// app.listen();
 // app.listen(5000, () => console.log('server Started at localhost 5000'));
